1de346b35ba725434e6fd62dd9ca79509e1948a3
boev <boev@rg.ru>
2024-01-03 12:53:57 +0200

    add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 import './styles/globals.css'
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         { children }
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
