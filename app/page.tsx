1d6519f61dd1484ce1ae4f545dcaeee6b935be89
boev <boev@rg.ru>
2024-01-03 19:10:48 +0200

    add prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: { author: { select: { name: true } } }
+  })
+
   return (
     <main>
-
+      {(feed || []).map(({ id, title }) => <div key={id}>{ title }</div>)}
     </main>
   )
 }
